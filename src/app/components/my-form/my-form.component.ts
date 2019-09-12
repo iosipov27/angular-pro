a1b3be1cda46d1fad88ff2ce7c0290df7d37fad4
ilia <iosipov83@gmail.com>
2019-09-12 21:58:43 +0300

    Added showMessage functionality

--- src/app/components/my-form/my-form.component.ts
+++ src/app/components/my-form/my-form.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output, AfterContentInit, ContentChild } from '@angular/core';
 import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import { RememberComponent } from '../remember/remember.component';
 
 @Component({
   selector: 'app-my-form',
   templateUrl: './my-form.component.html',
   styleUrls: ['./my-form.component.scss']
 })
-export class MyFormComponent {
+export class MyFormComponent implements AfterContentInit {
+
+  @ContentChild(RememberComponent) remember = RememberComponent;
+
+  public showMessage: boolean = false;
 
   @Output() submited = new EventEmitter();
 
   constructor() { }
+
+  ngAfterContentInit(): void {
+    if (this.remember) {
+      this.remember.checked.subscribe((val: boolean) => {
+        this.showMessage = val;
+      })
+    }
+    console.log(this.remember);
+  }
 
   onSubmit(val) {
     this.submited.emit(val);
