6fb42fd6b4d8643c8b2a803b9819409ad1470040
iosipov83 <iosipov83@gmail.com>
2019-09-25 15:38:53 +0300

    @HostBinding. border

--- src/app/directives/credit-card.directive.ts
+++ src/app/directives/credit-card.directive.ts
@@ -1,21 +1,24 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 
 
 @Directive({
   selector: '[credit-card]'
 })
-export class CreditCardDirective  {
+export class CreditCardDirective {
+
+  @HostBinding('style.border') border: string;
 
   @HostListener('input', ['$event']) onKeyDown(event: KeyboardEvent) {
-    console.log(event);
-
     const input = event.target as HTMLInputElement;
 
     let trimmed = input.value.replace(/\s+/g, '');
 
     if (trimmed.length > 10) {
-      trimmed =trimmed.substr(0 , 10);
+      trimmed = trimmed.substr(0, 10);
+      this.border = '2px solid red'
+    } else {
+      this.border = '';
     }
 
     input.value = trimmed;
