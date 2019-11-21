9b3f0382d8128176f5280d4b901f58b94bc54d97
iosipov83 <iosipov83@gmail.com>
2019-11-21 16:52:48 +0200

    Added ticking buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { merge, Observable, Subject, interval, NEVER, } from 'rxjs';
+import { mapTo, switchMap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  butt1$: Subject<any> = new Subject();
+  butt2$: Subject<any> = new Subject();
+
+
+  constructor() {
+
+    this.butt1$.pipe(switchMap(isTicking => {
+      return (isTicking) ? interval(1000) : NEVER
+    })).subscribe(data => {
+      console.log(data);
+    })
+
+  }
+
+  run() {
+    this.butt1$.next(true);
+  }
+
+  stop() {
+    this.butt1$.next(false);
+  }
 }
