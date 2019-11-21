3750be7d96b1b45b45a141a3b314db451f1ff259
iosipov83 <iosipov83@gmail.com>
2019-11-21 16:32:10 +0200

    fix proj building

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule }   from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { MyFormComponent } from './components/my-form/my-form.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyFormComponent,
-    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
