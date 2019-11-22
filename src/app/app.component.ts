c2ac0bd8a441377d679d21a85825c98db2748106
ilia <iosipov83@gmail.com>
2019-11-22 16:12:15 +0200

    specific value from specific observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { merge, Observable, Subject, interval, NEVER, } from 'rxjs';
-import { mapTo, switchMap, scan, shareReplay, startWith, map } from 'rxjs/operators';
+import { mapTo, switchMap, scan, shareReplay, startWith, map, pluck } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -26,7 +26,18 @@ export class AppComponent {
   )
 
   constructor() {
-    const initialState = {
+
+    interface Count {
+      isTicking: boolean,
+      isKeking: number,
+    }
+
+    const counterStateKeys = {
+      isTicking: 'isTicking',
+      isKeking: 'isKeking'
+    }
+
+    const initialState: Count = {
       isTicking: false,
       isKeking: 0
     }
@@ -34,11 +45,13 @@ export class AppComponent {
     const comands$ =
       this.counterState$.pipe(
         startWith(initialState),
-        scan((state, command) => ({ ...state, ...command })),
+        scan((state, command) => ({ ...state, ...command })),)
 
-      )
-    comands$.pipe(map(state => state.isKeking))
-      .subscribe(console.log);
+    const counts$ = comands$.pipe(pluck<Count, number>(counterStateKeys.isKeking));
+    const isSounts$ = comands$.pipe(pluck<Count, boolean>(counterStateKeys.isTicking));
+
+    isSounts$.subscribe(console.log);
+    counts$.subscribe(console.log);
   }
 
   run() {
